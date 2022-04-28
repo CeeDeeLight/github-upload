7c676a973ffb3799979e62fbaf332a01c45a3b9c
Delisha Cogdell <ceedeelight@yahoo.com>
2022-04-27 21:11:57 -0400

    initial-upload-commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+/* let message = 'Hello';
+
+function changeMessage(message) {
+    message = 'Hi!';
+}
+
+changeMessage(message);
+
+alert(message);
+
+let person = {
+    name: 'Delisha',
+    age: 47,
+    fullTime: true
+};
+
+function incrementAge(person) {
+    person.age++;
+};
+
+incrementAge(person);
+alert(person.age); */
+
+
+//Go to MDN for Standard Built in Objects. String Objects below:
+/* let currentTime = new Date();
+alert(currentTime.toDateString() ); */
+
+/*let s = 'Hello';
+alert(s.charAt(0) );*/
+
+
+//number objects   Number.NaN, Number.MIN_VALUE, Number.isNaN
+
+
+
+
+
